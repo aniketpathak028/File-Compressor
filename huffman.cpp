2692dabe49f77eb42dc9495c57f85bded778d900
Aniket Pathak <aniketpathak028@gmail.com>
2023-01-19 10:14:56 +0530

    huffman createArr

--- huffman.cpp
+++ huffman.cpp
@@ -1,1 +1,11 @@
 #include "huffman.hpp"
+
+void huffman::createArr()
+{
+    for (int i = 0; i < 128; i++)
+    {
+        arr.push_back(new Node());
+        arr[i]->data = i;
+        arr[i]->freq = 0;
+    }
+}
