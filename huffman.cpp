51aeb6dc154ac7b04e60631c3d29172c06aae144
Aniket Pathak <aniketpathak028@gmail.com>
2023-01-19 10:15:42 +0530

    huffman traverse

--- huffman.cpp
+++ huffman.cpp
@@ -9,3 +9,16 @@ void huffman::createArr()
         arr[i]->freq = 0;
     }
 }
+
+void huffman::traverse(Node *r, string str)
+{
+    if (r->left == NULL && r->right == NULL)
+    {
+        r->code = str;
+        return;
+    }
+
+    traverse(r->left, str + '0');
+    traverse(r->right, str + '1');
+}
+
